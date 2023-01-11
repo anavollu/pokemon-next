b066264c7fc4093950ae47f71363cc89674aef46
anavollu <anavollu@id.uff.br>
2023-01-11 13:03:28 -0300

    switch to regex and capitalize first letter for all words

--- src/utils/capitalizeFirstLetter.js
+++ src/utils/capitalizeFirstLetter.js
@@ -1,5 +1,7 @@
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.replace(/\b[a-z](?=[a-z])/g, function (letter) {
+    return letter.toUpperCase();
+  });
 }
 
 export default capitalizeFirstLetter;
